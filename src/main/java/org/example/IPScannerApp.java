73ca12029bd1f6af221fdd02b467063d218ada85
Воронков Никита <nikitavoronkov@yandex.ru>
2023-11-01 11:18:29 +0300

    Global: refactor structure add new classes

--- src/main/java/org/example/IPScannerApp.java
+++ src/main/java/org/example/IPScannerApp.java
@@ -2,18 +2,66 @@ package org.example;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.JavalinRenderer;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Objects;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class IPScannerApp {
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7000);
+        Javalin app = Javalin.create(config -> {
+                    config.addStaticFiles("/static", Location.CLASSPATH);
+                    JavalinRenderer.register(JavalinThymeleaf.INSTANCE);
+                })
+                .get("/", ctx -> {
+                    try {
+                        ctx.render("/templates/home.html", model("ip", "", "threadNum", 1));
+                    } catch (Exception e) {
+                        ctx.redirect("/", HttpStatus.forStatus(400));
+                    }
+                })
+                .start(8080);
+        app.post("/scan", ctx -> {
+            try {
+                String IP = ctx.formParam("ip");
+                int threadNum = Integer.parseInt(ctx.formParam("threadNum"));
+                String fileName = File.createTempFile("IPAddresses_Domains_", ".txt").getName();
+                List<String> result = IPScannerManager.scan(IP, threadNum, fileName);
+                ctx.render("/templates/home.html", model(
+                        "ip", IP
+                        , "threadNum", threadNum
+                        , "filename", fileName
+                        , "addresses", result.isEmpty() ? "empty" : result)
+                );
+            } catch (Exception e) {
+                ctx.redirect("/", HttpStatus.forStatus(400));
+            }
+        });
 
-        app.get("/", IPScannerApp::getHomepage);
-        app.post("/startScan", IPScannerApp::startScan);
+        app.get("/download/{filename}", ctx -> {
+            try {
+                File localFile = new File(ctx.pathParam("filename"));
+                InputStream inputStream = new BufferedInputStream(new FileInputStream(localFile));
+                ctx.header("Content-Disposition", "attachment; filename=\"" + localFile.getName() + "\"");
+                ctx.header("Content-Length", String.valueOf(localFile.length()));
+                ctx.result(inputStream);
+            } catch (Exception e) {
+                ctx.redirect("/", HttpStatus.forStatus(400));
+            }
+        });
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            app.stop();
-        }));
+        app.error(400, "html", ctx -> {
+            ctx.result("BAD REQUEST!");
+        });
     }
 
     private static void getHomepage(Context ctx) {
@@ -22,7 +70,9 @@ public class IPScannerApp {
 
     private static void startScan(Context ctx) {
         String ipRange = ctx.formParam("ipRange");
-        int numThreads = Integer.parseInt(ctx.formParam("numThreads"));
+        int numThreads = Integer.parseInt(Objects.requireNonNull(ctx.formParam("numThreads")));
+
+
 
         // Implement IP scanning logic here
         // Use Apache Http Client for making requests to IP addresses
--- src/main/java/org/example/IPScannerManager.java
+++ src/main/java/org/example/IPScannerManager.java
@@ -1,46 +1,153 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import inet.ipaddr.IPAddress;
+import inet.ipaddr.IPAddressSeqRange;
+import inet.ipaddr.IPAddressString;
+import org.apache.http.HttpResponseInterceptor;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.ManagedHttpClientConnection;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpCoreContext;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateParsingException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class IPScannerManager {
-    private final String[] ipAddresses; // Массив IP-адресов для сканирования
-    private final int numThreads; // Количество потоков для сканирования
-    private final ExecutorService executorService;
+    public static final String PEER_CERTIFICATES = "PEER_CERTIFICATES";
+    public static List<String> scan(String ipRange, int threadNum, String filename) throws IOException, InterruptedException {
+        FileWriter fw;
+        fw = new FileWriter(filename);
+        BufferedWriter bw = new BufferedWriter(fw);
 
-    public IPScannerManager(String[] ipAddresses, int numThreads) {
-        this.ipAddresses = ipAddresses;
-        this.numThreads = numThreads;
-        this.executorService = Executors.newFixedThreadPool(numThreads);
+        IPAddressSeqRange startIPAddress = new IPAddressString(ipRange).getSequentialRange();
+        List<IPAddress> addresses = new ArrayList<>();
+        startIPAddress.iterator().forEachRemaining(addresses::add);
+        List<String> result = Collections.synchronizedList(new ArrayList<>());
+        List<List<IPAddress>> addressesGroups = splitArrayList(addresses, Math.max(addresses.size()/threadNum, 1));
+        List<Thread> threads = new ArrayList<>();
+
+        for(List<IPAddress> groupAddresses: addressesGroups){
+            threads.add(new Thread(() -> {
+                for(IPAddress ipAddress: groupAddresses){
+                    try {
+                        scanIpAddress(ipAddress.toString(), bw, result);
+                    } catch (Exception e) {
+                        System.err.println("Exception handled while scan IP: " + ipAddress.toString());
+                    }
+                }
+            }));
+            threads.get(threads.size()-1).start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        bw.close();
+        fw.close();
+        return result;
     }
 
-    public void startScanning() {
-        int addressesPerThread = ipAddresses.length / numThreads;
-
-        for (int i = 0; i < numThreads; i++) {
-            int startIdx = i * addressesPerThread;
-            int endIdx = (i == numThreads - 1) ? ipAddresses.length : startIdx + addressesPerThread;
-            String[] addressesToScan = Arrays.copyOfRange(ipAddresses, startIdx, endIdx);
-
-            Runnable task = new IPScannerTask(addressesToScan);
-            executorService.execute(task);
+    public static <T> List<List<T>> splitArrayList(List<T> source, int chunkSize) {
+        List<List<T>> destination = new ArrayList<>();
+        for (int i = 0; i < source.size(); i += chunkSize) {
+            int end = Math.min(i + chunkSize, source.size());
+            destination.add(source.subList(i, end));
         }
+        return destination;
+    }
 
-        executorService.shutdown();
+    private static void scanIpAddress(String ipAddress, BufferedWriter bw, List<String> result) throws IOException, CertificateParsingException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        CloseableHttpClient httpClient = null;
         try {
-            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+            HttpResponseInterceptor certificateInterceptor = (httpResponse, context) -> {
+                ManagedHttpClientConnection routedConnection = (ManagedHttpClientConnection)context.getAttribute(HttpCoreContext.HTTP_CONNECTION);
+                SSLSession sslSession = routedConnection.getSSLSession();
+                if (sslSession != null) {
+                    Certificate[] certificates = sslSession.getPeerCertificates();
+                    context.setAttribute(PEER_CERTIFICATES, certificates);
+                }
+            };
+            final TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+            final SSLContext sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+            final SSLConnectionSocketFactory syslog =
+                    new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+            final Registry<ConnectionSocketFactory> socketFactoryRegistry =
+                    RegistryBuilder.<ConnectionSocketFactory> create()
+                            .register("https", syslog)
+                            .register("http", new PlainConnectionSocketFactory())
+                            .build();
+
+            final BasicHttpClientConnectionManager connectionManager =
+                    new BasicHttpClientConnectionManager(socketFactoryRegistry);
+
+            httpClient = HttpClients
+                    .custom()
+                    .setConnectionManager(connectionManager)
+                    .addInterceptorLast(certificateInterceptor)
+                    .build();
+
+            HttpGet httpget = new HttpGet("https://" + ipAddress);
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            HttpContext context = new BasicHttpContext();
+            httpClient.execute(httpget, context);
+
+            Certificate[] peerCertificates = (Certificate[])context.getAttribute(PEER_CERTIFICATES);
+
+            for (Certificate certificate : peerCertificates){
+                X509Certificate real = (X509Certificate) certificate;
+                if(real.getSubjectAlternativeNames() != null)
+                    real.getSubjectAlternativeNames().forEach(objects -> {
+                        if(objects != null)
+                            objects.forEach(domain -> {
+                                if(domain instanceof String) {
+                                    saveDomainToFile(ipAddress, (String) domain);
+                                    result.add("IP: " + ipAddress + ", Domain: " + domain);
+                                }
+                            });
+                    });
+            }
+        } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+            throw e;
+        } finally {
+            assert httpClient != null;
+            httpClient.close();
         }
     }
 
-    public static void main(String[] args) {
-        String[] ipAddresses = {"example1.com", "example2.com", "example3.com"}; // Замените на список IP-адресов
-        int numThreads = 2; // Количество потоков
-
-        IPScannerManager manager = new IPScannerManager(ipAddresses, numThreads);
-        manager.startScanning();
+    private static void saveDomainToFile(String ipAddress, String domain) {
+        try (FileWriter writer = new FileWriter("domains.txt", true);
+             BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+            bufferedWriter.write("IP Address: " + ipAddress + ", Domain: " + domain);
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
